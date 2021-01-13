5410f0ed2b373167e8221790c5186ed30169d2d3
Aaryatech <code@atsinfosoft.com>
2021-01-13 11:54:15 +0530

    Anmol

--- src/main/java/com/monginis/ops/constant/Constant.java
+++ src/main/java/com/monginis/ops/constant/Constant.java
@@ -2,55 +2,56 @@ package com.monginis.ops.constant;
 
 public class Constant {
 
-	 //public static final String URL = "http://localhost:8097/";
-	 public static final String URL = "http://132.148.148.215:8080/demoapi/";
-	
-	//PATNA PHASE2--------
-	
-	public static final String ITEM_IMAGE_URL = "http://132.148.143.124:8080/uploads/ITEM/";
+	// public static final String URL = "http://localhost:8097/";
 
-	public static final String SP_CAKE_FOLDER="http://132.148.143.124:8080/uploads/SPCAKE/";
-	
-	public static final String SPCAKE_IMAGE_URL = "http://132.148.143.124:8080/uploads/MSPCAKE/";
+	public static final String URL = "http://132.148.148.215:8080/demoapi/";
 
-	
-	public static final String PHOTO_CAKE_URL="http://132.148.143.124:8080/uploads/CUSTCHOICEPHOTOCAKE/";
+	public static final String ITEM_IMAGE_URL = "http://132.148.148.215:8080/uploads/ITEM/";
 
-	public static final String FR_IMAGE_URL="http://132.148.143.124:8080/uploads/FR/";
-	
-	public static final String GVN_IMAGE_URL="http://132.148.143.124:8080/uploads/GVN/";
+	public static final String SP_CAKE_FOLDER = "http://132.148.148.215:8080/uploads/SPCAKE/";
 
-	//public static final String ReportURL ="http://localhost:8986/ops/";
-	public static final String ReportURL ="http://132.148.143.124:8080/ops/";
-	public static final String MESSAGE_IMAGE_URL="http://132.148.143.124:8080/uploads/MSG/";
+	public static final String SPCAKE_IMAGE_URL = "http://132.148.148.215:8080/uploads/MSPCAKE/";
 
-	public static final String LOGIS_BILL_URL ="http://132.148.143.124:8080/uploads/MSPCAKE/";
+	public static final String PHOTO_CAKE_URL = "http://132.148.148.215:8080/uploads/CUSTCHOICEPHOTOCAKE/";
 
-	public static final String REPORT_SAVE = "/opt/apache-tomcat-8.5.6/webapps/uploads/Report.pdf";
-	
-	public static final String BILL_REPORT_PATH = "/opt/apache-tomcat-8.5.6/webapps/uploads/reportbill.pdf";
-	public static final String CRN_REPORT_PATH = "/opt/apache-tomcat-8.5.6/webapps/uploads/crn.pdf";
-	public static final String SELL_REPORT_PATH = "/opt/apache-tomcat-8.5.6/webapps/uploads/sellreport.pdf";
+	public static final String FR_IMAGE_URL = "http://132.148.148.215:8080/uploads/FR/";
 
-	public static final String FACTORYNAME = "Monginis";
-	public static final String FACTORYADDRESS = "Patna";
+	public static final String GVN_IMAGE_URL = "http://132.148.148.215:8080/uploads/GVN/";
+
+	// public static final String ReportURL ="http://localhost:8986/ops/";
+	public static final String ReportURL = "http://132.148.148.215:8080/ops/";
+	public static final String MESSAGE_IMAGE_URL = "http://132.148.148.215:8080/uploads/MSG/";
+
+	public static final String LOGIS_BILL_URL = "http://132.148.148.215:8080/uploads/MSPCAKE/";
+
+	public static final String REPORT_SAVE = "/home/devour/apache-tomcat-9.0.12/webapps/uploads/Report.pdf";
+	// public static final String REPORT_SAVE = "/home/ats-12/report.pdf";
+
+	public static final String FACTORYNAME = "DEVOUR FOODS LLP";
+
+	public static final String FACTORYADDRESS = "Village-Fatehpur,Dist-Patna";
+
+	public static final String BILL_REPORT_PATH = "/home/devour/apache-tomcat-9.0.12/webapps/uploads/reportbill.pdf";
+	public static final String CRN_REPORT_PATH = "/home/devour/apache-tomcat-9.0.12/webapps/uploads/crn.pdf";
+	public static final String SELL_REPORT_PATH = "/home/devour/apache-tomcat-9.0.12/webapps/uploads/sellreport.pdf";
+
 	public static final String FACTORYGSTIN = "-";
 	public static final String CITY = "Patna";
 	public static final String STATE = "Bihar";
-	
 
-	public static Integer dayOfMonthEnd=1;
+	public static Integer dayOfMonthEnd = 1;
 
-	public static Integer DEL_STATUS=1;
-	
-	public static int FR_IMAGE_TYPE=1;
-	
-	public static int SPCAKE_IMAGE_TYPE=4;
-	
-	public static int CUST_CHIOICE_IMAGE_TYPE=5;
-	
-	public static int GVN_IMAGE_TYPE=6;
+	public static Integer DEL_STATUS = 1;
 
-	
-	
+	public static int FR_IMAGE_TYPE = 1;
+	/*
+	 * public static int ITEM_IMAGE_TYPE=2; public static int MESSAGE_IMAGE_TYPE=3;
+	 */
+
+	public static int SPCAKE_IMAGE_TYPE = 4;
+
+	public static int CUST_CHIOICE_IMAGE_TYPE = 5;
+
+	public static int GVN_IMAGE_TYPE = 6;
+
 }
